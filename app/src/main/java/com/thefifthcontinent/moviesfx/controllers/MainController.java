557f6114cb04220b2596405802202d7049dc0124
Richard Gladman <richard@thefifthcontinent.com>
2025-05-16 19:00:02 +0100

    Show main window and exit program

--- app/src/main/java/com/thefifthcontinent/moviesfx/controllers/MainController.java
+++ app/src/main/java/com/thefifthcontinent/moviesfx/controllers/MainController.java
@@ -1,6 +1,58 @@
 package com.thefifthcontinent.moviesfx.controllers;
 
+import java.lang.management.PlatformLoggingMXBean;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 public class MainController
 {
-
+	@FXML
+	private MenuItem menu_actors;
+	
+	@FXML
+	private MenuItem menu_directors;
+	
+	@FXML
+	private MenuItem menu_movies;
+	
+	@FXML
+	private MenuItem menu_about;
+	
+	@FXML
+	public void onSaveMoviesClicked()
+	{
+		
+	}
+	
+	@FXML
+	public void onQuitClicked()
+	{
+		Platform.exit();
+	}
+	
+	@FXML
+	public void onActorsClicked()
+	{
+		
+	}
+	
+	@FXML
+	public void onDirectorsClicked()
+	{
+		
+	}
+	
+	@FXML
+	public void onMoviesClicked()
+	{
+		
+	}
+	
+	@FXML
+	public void onAboutClicked()
+	{
+		
+	}
 }
