8791231ffb3d764a6660c49beb15f03a1eedf585
Richard Gladman <richard@thefifthcontinent.com>
2025-05-18 11:25:25 +0100

    Search and filter actor list

--- app/src/main/java/com/thefifthcontinent/moviesfx/controllers/ActorController.java
+++ app/src/main/java/com/thefifthcontinent/moviesfx/controllers/ActorController.java
@@ -1,16 +1,19 @@
 package com.thefifthcontinent.moviesfx.controllers;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 
 import com.thefifthcontinent.moviesfx.model.Actor;
 import com.thefifthcontinent.moviesfx.util.DataHandler;
 
+import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -25,11 +28,17 @@ public class ActorController
 	@FXML
 	private Button addButton;
 	
+	@FXML
+	private TextField searchTextField;
+	
+	private FilteredList<Actor> filteredList;
+	
 	public void initialize()
 	{
-		SortedList<Actor> sortedList = new SortedList<>(DataHandler.getInstance().getActors(), 
-														(o1, o2) -> o1.getName().compareTo(o2.getName()));
+		filteredList = new FilteredList<>(DataHandler.getInstance().getActors(), (i) -> !i.getName().isEmpty());
+		SortedList<Actor> sortedList = new SortedList<>(filteredList, (o1, o2) -> o1.getName().compareTo(o2.getName()));
 		actorsListView.setItems(sortedList);
+		actorsListView.getSelectionModel().selectFirst();
 	}
 	
 	@FXML
@@ -51,6 +60,26 @@ public class ActorController
 		} catch (IOException e) {
 			e.printStackTrace();
 		}    	
+	}
+	
+	@FXML
+	public void handleSearchButton()
+	{
+		Actor selectedActor = actorsListView.getSelectionModel().getSelectedItem();
+		Predicate<Actor> searchPredicate = (a) -> a.getName().toLowerCase().startsWith(searchTextField.getText().trim().toLowerCase());
+		Predicate<Actor> allPredicate = (a) -> !a.getName().isEmpty();
+		
+		if (searchTextField.getText().trim().isEmpty()) {
+			filteredList.setPredicate(allPredicate);
+		} else {
+			filteredList.setPredicate(searchPredicate);
+		}
+		
+		if (filteredList.contains(selectedActor)) {
+			actorsListView.getSelectionModel().select(selectedActor);
+		} else {
+			actorsListView.getSelectionModel().selectFirst();
+		}
 	}
 	
 	public static Stage getStage()
