69db9cc583126ce5fd8f8419689b9f5b19730bac
Richard Gladman <richard@thefifthcontinent.com>
2025-05-18 13:08:46 +0100

    Handle delete actor

--- app/src/main/java/com/thefifthcontinent/moviesfx/controllers/ActorController.java
+++ app/src/main/java/com/thefifthcontinent/moviesfx/controllers/ActorController.java
@@ -1,9 +1,11 @@
 package com.thefifthcontinent.moviesfx.controllers;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 import com.thefifthcontinent.moviesfx.model.Actor;
+import com.thefifthcontinent.moviesfx.model.Movie;
 import com.thefifthcontinent.moviesfx.util.DataHandler;
 
 import javafx.collections.transformation.FilteredList;
@@ -11,7 +13,10 @@ import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -60,6 +65,37 @@ public class ActorController
 		} catch (IOException e) {
 			e.printStackTrace();
 		}    	
+	}
+	
+	@FXML
+	public void handleDeleteButton()
+	{
+		Actor selectedActor = actorsListView.getSelectionModel().getSelectedItem();
+		
+		boolean okToDelete = true;
+		for (Movie movie: DataHandler.getInstance().getMovies()) {
+			if (movie.getActors().contains(selectedActor)) {
+				okToDelete = false;
+				break;
+			}
+		}
+				
+		if (okToDelete) {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setTitle("Delete Actor");
+			alert.setHeaderText("Delete actor: " + selectedActor.getName());
+			alert.setContentText("Are you sure? Press OK to confirm or cancel to back out.");
+			
+			Optional<ButtonType> result = alert.showAndWait();
+			if (result.isPresent() && result.get() == ButtonType.OK) {
+				DataHandler.getInstance().getActors().remove(selectedActor);
+			}
+		} else {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Delete Actor");
+			alert.setContentText("Cannot delete " + selectedActor.getName() + ". Present in movie.");
+			alert.showAndWait();
+		}
 	}
 	
 	@FXML
